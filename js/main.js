d0e1502fc4c4348ae2e1f2113124afebe66ac3fb
Leila.L <leila.lababsa@hotmail.fr>
2017-07-07 11:56:18 +0200

    work on clickable images

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ var pseudo= document.formulaire.text.value;
 
 document.getElementById('result').innerHTML = "Bienvenue :" + " " + pseudo;
 }
+
+var gamePictures = document.getElementsByClassName("gamePicture");
+for (i=0; i< gamePictures.length; i++) {
+ gamePictures[i].onclick = function () {
+ gamePictures[i].getAttribute("alt");
+ alert();
+ }
+}
