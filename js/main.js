fb22514923b47f814834b86c362d3cc581139a46
Leila.L <leila.lababsa@hotmail.fr>
2017-07-10 11:35:49 +0200

    solved problem of loop

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-/***** pseudo ******/
+/***** entrer pseudo ******/
 function donnees() {
 var pseudo= document.formulaire.text.value;
 
 document.getElementById('result').innerHTML = "Bienvenue :" + " " + pseudo;
 }
 
-/******* images cliquable ********/
+/******* images alt ********/
 var gamePictures = document.getElementsByClassName("gamePicture");
 for (i=0; i< gamePictures.length; i++) {
  gamePictures[i].onclick = function () {
- gamePictures[i].getAttribute("alt");
- alert();
- }
+ game(this.alt);
+ };
 }
 
-/******* random ordinateur ********/
+var pointUser = 0;
+var pointComputer = 0;
+function game (user) {
 
+var user;
 var computer;
 
-while (pointUser < 3 && pointComputer <  3) {
-  user = prompt("Pierre, feuille ou ciseaux");
+if (pointUser < 3 && pointComputer <  3) {
   computer = Math.floor((Math.random() * 3) + 1);
 
   if (computer === 1) {
@@ -31,3 +32,56 @@ while (pointUser < 3 && pointComputer <  3) {
   else {
     computer="ciseaux";
   }
+
+  document.getElementById("choixJoueur").innerHTML ="Vous: " + user;
+  document.getElementById("choixOrdi").innerHTML ="l'Ordinateur: " + computer;
+
+  if (user === computer) {
+    document.getElementById("resultat").innerHTML ="égalité";
+  }
+
+  else if (user === "pierre" && computer === "ciseaux") {
+    document.getElementById("resultat").innerHTML ="1 point pour toi";
+    pointUser++;
+  }
+
+
+  else if (user === "feuille" && computer === "pierre") {
+  document.getElementById("resultat").innerHTML ="1 point pour toi";
+    pointUser++;
+  }
+
+  else if (user === "feuille" && computer === "ciseaux") {
+      document.getElementById("resultat").innerHTML ="perdu";
+      pointComputer++;
+        }
+
+        else if (user === "pierre" && computer === "feuille") {
+      document.getElementById("resultat").innerHTML ="perdu";
+      pointComputer++;
+        }
+
+        else if (user === "ciseaux" && computer === "feuille") {
+      document.getElementById("resultat").innerHTML ="1 point pour toi";
+      pointUser++;
+      }
+
+  else if (user === "ciseaux" && computer === "pierre") {
+    document.getElementById("resultat").innerHTML ="perdu";
+    pointComputer++;
+  }
+
+  document.getElementById("pointJoueur").innerHTML = "Vous: " + pointUser;
+  document.getElementById("pointOrdi").innerHTML = "l'Ordinateur: " + pointComputer;
+
+
+  if (pointUser === 3) {
+    document.getElementById("winner").innerHTML ="Tu as gagné la partie !";
+  }
+
+  else {
+    document.getElementById("winner").innerHTML ="Tu as perdu la partie !";
+  }
+
+}
+}
