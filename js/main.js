2e2003558b4166fd78d13978dd841bc50c4b0709
Leila.L <leila.lababsa@hotmail.fr>
2017-07-10 14:34:49 +0200

    style + position of contents html

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var pseudo= document.formulaire.text.value;
 document.getElementById('result').innerHTML = "Bienvenue :" + " " + pseudo;
 }
 
-/******* images alt ********/
+/******* recuparation images alt ********/
 var gamePictures = document.getElementsByClassName("gamePicture");
 for (i=0; i< gamePictures.length; i++) {
  gamePictures[i].onclick = function () {
@@ -15,10 +15,10 @@ for (i=0; i< gamePictures.length; i++) {
 
 var pointUser = 0;
 var pointComputer = 0;
-function game (user) {
-
 var user;
 var computer;
+
+function game (user) {
 
 if (pointUser < 3 && pointComputer <  3) {
   computer = Math.floor((Math.random() * 3) + 1);
@@ -33,49 +33,41 @@ if (pointUser < 3 && pointComputer <  3) {
     computer="ciseaux";
   }
 
-  document.getElementById("choixJoueur").innerHTML ="Vous: " + user;
   document.getElementById("choixOrdi").innerHTML ="l'Ordinateur: " + computer;
 
   if (user === computer) {
-    document.getElementById("resultat").innerHTML ="égalité";
   }
 
   else if (user === "pierre" && computer === "ciseaux") {
-    document.getElementById("resultat").innerHTML ="1 point pour toi";
     pointUser++;
   }
 
 
   else if (user === "feuille" && computer === "pierre") {
-  document.getElementById("resultat").innerHTML ="1 point pour toi";
     pointUser++;
   }
 
   else if (user === "feuille" && computer === "ciseaux") {
-      document.getElementById("resultat").innerHTML ="perdu";
       pointComputer++;
         }
 
         else if (user === "pierre" && computer === "feuille") {
-      document.getElementById("resultat").innerHTML ="perdu";
       pointComputer++;
         }
 
         else if (user === "ciseaux" && computer === "feuille") {
-      document.getElementById("resultat").innerHTML ="1 point pour toi";
       pointUser++;
       }
 
   else if (user === "ciseaux" && computer === "pierre") {
-    document.getElementById("resultat").innerHTML ="perdu";
     pointComputer++;
   }
 
-  document.getElementById("pointJoueur").innerHTML = "Vous: " + pointUser;
-  document.getElementById("pointOrdi").innerHTML = "l'Ordinateur: " + pointComputer;
+  document.getElementById("pointJoueur").innerHTML = pointUser + " - ";
+  document.getElementById("pointOrdi").innerHTML = pointComputer;
+}
 
-
-  if (pointUser === 3) {
+  else if (pointUser === 3) {
     document.getElementById("winner").innerHTML ="Tu as gagné la partie !";
   }
 
@@ -83,5 +75,5 @@ if (pointUser < 3 && pointComputer <  3) {
     document.getElementById("winner").innerHTML ="Tu as perdu la partie !";
   }
 
-}
+
 }
