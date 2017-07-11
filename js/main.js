33660d3554c76e0c528e6a84260131cf54090d38
Leila.L <leila.lababsa@hotmail.fr>
2017-07-11 11:36:16 +0200

    checked indent+comments

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 /***** entrer pseudo ******/
 function donnees() {
-var pseudo= document.formulaire.text.value;
+  var pseudo = document.formulaire.text.value;
 
-document.getElementById('result').innerHTML = "Bienvenue :" + " " + pseudo;
+  document.getElementById('result').innerHTML = "Bienvenue :" + " " + pseudo;
 }
 
 /******* recuparation images alt ********/
 var gamePictures = document.getElementsByClassName("gamePicture");
-for (i=0; i< gamePictures.length; i++) {
- gamePictures[i].onclick = function () {
- game(this.alt);
- };
+for (i = 0; i < gamePictures.length; i++) {
+  gamePictures[i].onclick = function() {
+    game(this.alt);
+  };
 }
 
 var pointUser = 0;
@@ -18,72 +18,55 @@ var pointComputer = 0;
 var user;
 var computer;
 
-function game (user) {
+function game(user) {
 
-if (pointUser < 3 && pointComputer <  3) {
-  computer = Math.floor((Math.random() * 3) + 1);
+  if (pointUser < 3 && pointComputer < 3) {
+    computer = Math.floor((Math.random() * 3) + 1);
 
-  if (computer === 1) {
-    computer="pierre";
-    document.getElementById("jeu").src ="img/pierre.png"
-    // document.getElementById("jeu").style.transform = "translateY(-30px)";
-    setTimeout(function(){document.getElementById("jeu").style.transform = "translateY(-110px)";
-},1000);
-  }
-  else if (computer === 2) {
-    computer="feuille";
-    document.getElementById("jeu").src ="img/feuille.png"
-    // document.getElementById("jeu").style.transform = "translateY(-30px)";
-    setTimeout(function(){document.getElementById("jeu").style.transform = "translateY(-110px)";
-  },1000);
-  }
-  else {
-    computer="ciseaux";
-    document.getElementById("jeu").src ="img/ciseaux.png"
-    // document.getElementById("jeu").style.transform = "translateY(-30px)";
-    setTimeout(function(){document.getElementById("jeu").style.transform = "translateY(-110px)";
-  },1000);
-  }
-
-
-  if (user === computer) {
-  }
-
-  else if (user === "pierre" && computer === "ciseaux") {
-    pointUser++;
-  }
+    if (computer === 1) {
+      computer = "pierre";
+      document.getElementById("jeu").src = "img/pierre.png"
+      // document.getElementById("jeu").style.transform = "translateY(-30px)";
+      setTimeout(function() {
+        document.getElementById("jeu").style.transform = "translateY(-110px)";
+      }, 1000);
+    } else if (computer === 2) {
+      computer = "feuille";
+      document.getElementById("jeu").src = "img/feuille.png"
+      // document.getElementById("jeu").style.transform = "translateY(-30px)";
+      setTimeout(function() {
+        document.getElementById("jeu").style.transform = "translateY(-110px)";
+      }, 1000);
+    } else {
+      computer = "ciseaux";
+      document.getElementById("jeu").src = "img/ciseaux.png"
+      // document.getElementById("jeu").style.transform = "translateY(-30px)";
+      setTimeout(function() {
+        document.getElementById("jeu").style.transform = "translateY(-110px)";
+      }, 1000);
+    }
 
 
-  else if (user === "feuille" && computer === "pierre") {
-    pointUser++;
-  }
-
-  else if (user === "feuille" && computer === "ciseaux") {
-      pointComputer++;
-        }
-
-        else if (user === "pierre" && computer === "feuille") {
-      pointComputer++;
-        }
-
-        else if (user === "ciseaux" && computer === "feuille") {
+    if (user === computer) {} else if (user === "pierre" && computer === "ciseaux") {
       pointUser++;
-      }
+    } else if (user === "feuille" && computer === "pierre") {
+      pointUser++;
+    } else if (user === "feuille" && computer === "ciseaux") {
+      pointComputer++;
+    } else if (user === "pierre" && computer === "feuille") {
+      pointComputer++;
+    } else if (user === "ciseaux" && computer === "feuille") {
+      pointUser++;
+    } else if (user === "ciseaux" && computer === "pierre") {
+      pointComputer++;
+    }
 
-  else if (user === "ciseaux" && computer === "pierre") {
-    pointComputer++;
-  }
-
-  document.getElementById("pointJoueur").innerHTML = pointUser + " - ";
-  document.getElementById("pointOrdi").innerHTML = pointComputer;
-}
-
-  else if (pointUser === 3) {
-    document.getElementById("winner").innerHTML ="Tu as gagné la partie !";
-  }
-
-  else {
-    document.getElementById("winner").innerHTML ="Tu as perdu la partie!";
+    document.getElementById("pointJoueur").innerHTML = pointUser + " - ";
+    document.getElementById("pointOrdi").innerHTML = pointComputer;
+  } else if (pointUser === 3) {
+    document.getElementById("winner").innerHTML = "Tu as gagné la partie !";
+  } else {
+    document.getElementById("winner").innerHTML = "Tu as perdu la partie!";
   }
 
 }
